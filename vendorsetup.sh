9ba9052ee06c018c62ef57ed3e794fc4a430b193
kzsaaf3e <157870730+kzsaaf3e@users.noreply.github.com>
2024-04-28 13:26:34 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_Infinix-X680-user
-add_lunch_combo omni_Infinix-X680-userdebug
-add_lunch_combo omni_Infinix-X680-eng
+add_lunch_combo twrp_X680-user
+add_lunch_combo twrp_X680-userdebug
+add_lunch_combo twrp_X680-eng
